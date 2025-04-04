36524fc42e4a967b0ab7ba6bd686329c1c5cbe50
Vishal Kondle <71583944+vishalkondle-dev@users.noreply.github.com>
2025-04-04 22:25:36 +0530

    configured: prisma & postgresql

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { getPrisma } from "../lib/prisma";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: { DATABASE_URL: string };
+  // Variables: { userId: string };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const user = await prisma.user.create({
+    data: { email: String(Date.now()), name: "1", password: "1" },
+  });
+  return c.json(user);
+});
 
-export default app
+app.get("/users", async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const users = await prisma.user.findMany();
+  return c.json(users);
+});
+
+app.post("/users", async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const body = await c.req.json();
+  const user = await prisma.user.create({ data: body });
+  return c.json(user);
+});
+
+export default app;
